5f9a0e2f38e3c9c01c35f6ae489e088b3a27af80
adamcread <adamcread1@gmail.com>
2022-04-13 16:52:17 +0100

    remove test pipeline

--- instance_segmentation/configs/custom/detectors_instaboost.py
+++ instance_segmentation/configs/custom/detectors_instaboost.py
@@ -165,52 +165,20 @@ train_pipeline = [
         keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks', 'gt_semantic_seg']),
 ]
 
-test_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(type='Albu', 
-        transforms=train_transforms,
-        bbox_params=dict(
-            type='BboxParams',
-            format='pascal_voc',
-            label_fields=['gt_labels'],
-            min_visibility=0.0,
-            filter_lost_elements=True),
-        keymap={
-            'img': 'image',
-            'gt_masks': 'masks',
-            'gt_bboxes': 'bboxes'
-        },
-        update_pad_shape=False,
-        skip_img_without_anno=True),
-    dict(
-        type='MultiScaleFlipAug',
-        img_scale=(1333, 800),
-        flip=False,
-        transforms=[
-            dict(type='Resize', keep_ratio=True),
-            dict(type='RandomFlip', flip_ratio=0.5),
-            dict(type='Normalize', mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True),
-            dict(type='Pad', size_divisor=32),
-            dict(type='ImageToTensor', keys=['img']),
-            dict(type='Collect', keys=['img']),
-        ])
-]
-
 dataset_type = 'COCODataset'
 classes = ('person',)
 data = dict(
     train=dict(
-        img_prefix='../coco/images/val/',
-        seg_prefix='../coco/segmentations/val/',
+        img_prefix='../coco/images/train/',
+        seg_prefix='../coco/segmentations/train/',
         classes=classes,
-        ann_file='../coco/annotations/val_filtered.json',
+        ann_file='../coco/annotations/train_filtered.json',
         pipeline=train_pipeline),
     val=dict(
         img_prefix='../coco/images/val/',
         seg_prefix='../coco/segmentations/val/',
         classes=classes,
-        ann_file='../coco/annotations/val_filtered.json',
-        pipeline=test_pipeline)
+        ann_file='../coco/annotations/val_filtered.json')
 )
 
 runner = dict(type='EpochBasedRunner', max_epochs=5)
