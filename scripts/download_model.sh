fb63a20270e31f6957c795afbeaa0a88968efbbc
adamcread <adamcread1@gmail.com>
2022-05-06 14:14:43 +0100

    checkpoint folder

--- scripts/download_model.sh
+++ scripts/download_model.sh
@@ -4,11 +4,11 @@ echo "Note: available models are apple2orange, horse2zebra, photo2map, cezanne2p
 echo "Specified [$FILE]"
 
 URL=http://disi.unitn.it/~hao.tang/uploads/models/AttentionGAN/${FILE}_pretrained.tar.gz
-TAR_FILE=./checkpoints/${FILE}_pretrained.tar.gz
-TARGET_DIR=./checkpoints/${FILE}_pretrained/
+TAR_FILE=./${FILE}_pretrained.tar.gz
+TARGET_DIR=./${FILE}_pretrained/
 
 wget -N $URL -O $TAR_FILE
 
 mkdir -p $TARGET_DIR
-tar -zxvf $TAR_FILE -C ./checkpoints/
+tar -zxvf $TAR_FILE -C ./
 rm $TAR_FILE
